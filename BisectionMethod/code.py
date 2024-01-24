373678f75cad67f9e8edfcecf7378b7ac9281a0c
Your Name <you@example.com>
2024-01-24 15:07:47 +0530

    first commit

---
+++ BisectionMethod/code.py
@@ -0,0 +1,29 @@
+import math
+
+def bisect(f, a, b, absolute_error):
+    if f(a) * f(b) > 0:
+        return None
+    while (b - a) / 2 > absolute_error:
+        c = (a + b) / 2
+        if abs(f(c)) < absolute_error:
+            return c
+        elif f(b) * f(c) < 0:
+            a, b = c, b
+        else:
+            a, b = a, c
+
+    return (a + b) / 2
+function = input("Enter function: ")
+f = lambda x: eval(function)
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+
+absolute_error = float(input("Enter absolute error: "))
+
+root = bisect(f, a, b, absolute_error)
+if root is None:
+    print("\nRoot not found. Try again with different values.")
+else:
+    print("\nRoot: ", root)
+    print("f(root): ", f(root))
+
---
+++ README.md
@@ -0,0 +1,5 @@
+# Repo for Numerical Methods Lab
+
+## Contents
+* [Bisection Method](/BisectionMethod/)
+[image](/BisectionMethod/bisection.png)
